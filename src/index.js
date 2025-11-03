0637cf8cda4726ae015e7ba09658a3aea2cc4c9c
Shreyash kumar <shreyashkr0305@gmail.com>
2025-11-04 01:01:38 +0530

    util codes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import dotenv from "dotenv"
 
 import express from "express"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000);
+    console.log(`Server is running on ${process.env.PORT}`)
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed!!",err)
+})
+
+
+
+
 
 // const app=express()
 
